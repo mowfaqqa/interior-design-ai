f157ca5f85eb3e1aa1bc2e7c333aff7d94de3c4f
Musa Nazar <158774828+Musa-Nazar@users.noreply.github.com>
2025-08-09 16:07:15 +0100

    Update CreateLayoutMainSection.tsx

--- src/components/CreateLayout/CreateLayoutMainSection.tsx
+++ src/components/CreateLayout/CreateLayoutMainSection.tsx
@@ -8,8 +8,8 @@ const CreateLayoutMainSection = () => {
     value: "",
   });
   const xml: React.ReactElement = (
-    <main className="flex gap-[3.5rem] p-[3.5rem] bg-[#242426] h-[calc(100dvh-7rem)] relative">
-      <div className="w-[calc((100%-33.4rem)-3.5rem)]">
+    <main className="flex gap-[3.5rem] p-[3.5rem] bg-[#242426] h-[calc(100dvh-7rem)] relative max-lg:px-0 max-lg:justify-center max-lg:items-center max-lg:pb-[9rem] max-lg:min-h-[calc(100dvh-7rem)] max-lg:h-auto">
+      <div className="w-[calc((100%-33.4rem)-3.5rem)] max-lg:hidden">
         <input type="file" id="file" className="hidden" />
         <div className="flex justify-center items-center w-full h-full bg-[#1E1E1E] border-dashed border-[0.1rem] border-[#444444]">
           <span className="font-semibold text-[1.8rem] leading-[120%] tracking-[-.02em] text-white">
@@ -18,7 +18,7 @@ const CreateLayoutMainSection = () => {
         </div>
       </div>
       <CreateLayoutForm setImg={setImg} img={img} />
-      <div className="absolute top-[-1.6rem] left-[12.2rem] w-[22rem] pt-[2.3rem] pb-[0.7rem] rounded-[0.8rem] bg-[#979797] border border-[#444444] text-center z-[1]">
+      <div className="absolute top-[-1.6rem] left-[12.2rem] w-[22rem] pt-[2.3rem] pb-[0.7rem] rounded-[0.8rem] bg-[#979797] border border-[#444444] text-center z-[1] max-lg:hidden">
         <span className="fustat text-[1.2rem] text-white font-semibold leading-[1.2rem]">
           Créateur de rendu
         </span>
