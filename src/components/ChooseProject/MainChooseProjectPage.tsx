e1d48ad8d0b935c80c275d12409c0157281f89b6
Muwaffaq Aliyu <itzmuwaffaq@gmail.com>
2025-08-08 23:53:21 -0700

    added navigation to new project page

--- src/components/ChooseProject/MainChooseProjectPage.tsx
+++ src/components/ChooseProject/MainChooseProjectPage.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ScreenScale from "../ScreenScale";
 import MainPageWrapper from "../WelcomePage/MainPageWrapper";
 import OptionBox from "../WelcomePage/OptionBox";
@@ -6,16 +7,20 @@ const MainChooseProjectPage: React.FC = () => {
   const xml: React.ReactElement = (
     <MainPageWrapper head="Choisissez un type de projet">
       <ScreenScale className="!w-[69.3rem] flex gap-[2.5rem] h-[33.4rem] max-lg:flex-col max-lg:h-[17.5rem] max-lg:gap-[1.5rem]">
-        <OptionBox>
-          <span className="fustat font-bold text-[1.8rem] text-white text-center">
-            Résidentiel
-          </span>
-        </OptionBox>
-        <OptionBox>
-          <span className="fustat font-bold text-[1.8rem] text-white text-center">
-            Bureaux
-          </span>
-        </OptionBox>
+        <Link href={"/choose-interior-style"} passHref>
+          <OptionBox>
+            <span className="fustat font-bold text-[1.8rem] text-white text-center">
+              Résidentiel
+            </span>
+          </OptionBox>
+        </Link>
+        <Link href={"/choose-commercial-style"} passHref>
+          <OptionBox>
+            <span className="fustat font-bold text-[1.8rem] text-white text-center">
+              Bureaux
+            </span>
+          </OptionBox>
+        </Link>
       </ScreenScale>
     </MainPageWrapper>
   );
